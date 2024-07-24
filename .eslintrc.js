8a07158702b2f7adbaff443ba976ced0c4159f73
Mohamed Suja <mohamedsuja7n@gmail.com>
2024-07-24 17:54:31 +0530

    update

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,43 @@
+const { rules } = require('eslint-config-prettier');
+
 module.exports = {
-  root: true,
-  extends: '@react-native',
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: ['eslint:recommended', 'plugin:react/recommended', 'prettier'],
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: ['.eslintrc.{js,cjs}'],
+      parserOptions: {
+        sourceType: 'script',
+      },
+    },
+  ],
+  rules: {
+    'no-console': 'error',
+    'react/react-in-jsx-scope': 'off',
+    'spaced-comment': 'error',
+    quotes: ['error', 'single'],
+    'no-duplicate-imports': 'error',
+    'unused-imports/no-unused-imports-ts': 2,
+    'react/prop-types': 'off',
+  },
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: ['react', 'unused-imports'],
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
 };
