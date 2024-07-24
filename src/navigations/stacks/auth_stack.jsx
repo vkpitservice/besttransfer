042f83d429e7e06c4e06da4c1c2398609371259b
Mohamed Suja <mohamedsuja7n@gmail.com>
2024-07-24 13:30:42 +0530

    update

--- src/navigations/stacks/auth_stack.jsx
+++ src/navigations/stacks/auth_stack.jsx
@@ -1,16 +1,17 @@
 import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
-import LoginScreen from '../../screens/auth/login';
-
-import CreateAccount from '../../screens/auth/create_account';
 import Success from '@/screens/global/success';
+import OtpVerification from '@/screens/global/OtpVerification';
+import Login from '@/screens/auth/login';
+import CreateAccount from '@/screens/auth/create_account';
 
 const Stack = createStackNavigator();
 
 const AuthStack = () => {
   return (
     <Stack.Navigator initialRouteName='LoginScreen' screenOptions={{ headerShown: false }}>
-      <Stack.Screen name='LoginScreen' component={LoginScreen} />
+      <Stack.Screen name='LoginScreen' component={Login} />
+      <Stack.Screen name='OtpVerification' component={OtpVerification} />
       <Stack.Screen name='RegisterSuccessFullScreen' component={Success} />
       <Stack.Screen name='CreateAccountScreen' component={CreateAccount} />
     </Stack.Navigator>
---
+++ src/screens/global/OtpVerification/index.jsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native';
+import React from 'react';
+
+const OtpVerification = () => {
+  return (
+    <View>
+      <Text>O</Text>
+    </View>
+  );
+};
+
+export default OtpVerification;
